dccbdbb2e57e416d102d0211ba621c62f800fbb3
shimamaru <115210019+shimamaru@users.noreply.github.com>
2022-12-27 18:42:37 +0900

    atoms/AddTaskButton

---
+++ src/components/Example/AddTaskButton/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import plus from "../../../assets/svg/plus.svg";
+import COLOR from "../../../variables/color";
+
+const AddTaskButton = ({ onClick }) => {
+  return (
+    <StyledAddTaskButton onClick={onClick}>
+      <PlusButton src={plus} />
+      <ButtonText>タスクを追加</ButtonText>
+    </StyledAddTaskButton>
+  );
+};
+
+export default AddTaskButton;
+
+const StyledAddTaskButton = styled.div`
+  display: flex;
+  width: fit-content;
+  column-gap: 10px;
+  border-radius: 12px;
+  padding: 2px 6px;
+
+  &:hover {
+    background-color: #c5d8d2;
+  }
+`;
+
+const PlusButton = styled.img``;
+
+const ButtonText = styled.p`
+  color: ${COLOR.GREEN};
+  margin: 0;
+`;
+
+const StyledText = styled.div`
+  color: ${COLOR.WHITE};
+  ${TEXTS.S}
+`;
