1f8bd8bbb625289add1c7e7b266adf885c1bdde3
島谷　悠 <cfznj4sc2k@privaterelay.appleid.com>
2023-01-12 20:17:11 +0900

    ホバー時の軌道修正

--- src/components/Example/EditButton/index.jsx
+++ src/components/Example/EditButton/index.jsx
@@ -7,23 +7,35 @@ const SampleButton = ({ onClick }) => {
   return (
     <StyledButton onClick={onClick}>
       <StyledIcon src={pencil} />
+      <StyledShadow />
     </StyledButton>
   );
 };
 
 export default SampleButton;
+const StyledShadow = styled.div`
+  position: absolute;
+  height: 24px;
+  width: 24px;
+  border-radius: 50%;
+  top: 0px;
+`;
 
 const StyledButton = styled.button`
   padding: 0;
   border: none;
   height: 24px;
+  background-color: transparent;
+  position: relative;
+
+  &:hover {
+    ${StyledShadow} {
+      background-color: ${COLOR.LIGHT_GRAY};
+      opacity: 0.2;
+    }
+  }
 `;
 
 const StyledIcon = styled.img`
   cursor: pointer;
-
-  &:hover {
-    background-color: #dcdcdc;
-    border-radius: 50%;
-  }
 `;
