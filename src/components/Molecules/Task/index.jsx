e1529241b188fc8b257f93791641aa4dcc5c23a2
島谷　悠 <y.shimatani.22.10.658@4nonome.com>
2023-02-18 14:48:36 +0900

    Molecules/Taskの作成

---
+++ src/components/Molecules/Task/index.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Checkbox from "../../Atoms/Checkbox";
+import EditButton from "../../Atoms/EditButton";
+import SInput from "../../Atoms/Input";
+
+const Title = ({
+  onTaskChange,
+  onTaskComplete,
+  taskName = "",
+  defaultIsEditing = false,
+}) => {
+  const [isEditing, setIsEditing] = useState(defaultIsEditing);
+
+  const handleEditComplete = () => {
+    setIsEditing(false);
+  };
+
+  const handleEditButtonClick = () => {
+    setIsEditing(true);
+  };
+
+  return (
+    <StyledWrapper>
+      <StyledCheckboxWrapper>
+        <Checkbox onClick={onTaskComplete} />
+      </StyledCheckboxWrapper>
+      {isEditing ? (
+        <SInput onEditComplete={handleEditComplete} defaultValue={taskName} />
+      ) : (
+        <StyledNameAndButtonWrapper>
+          <StyledTaskName>{taskName}</StyledTaskName>
+          <StyledEditButtonWrapper>
+            <EditButton onClick={handleEditButtonClick} />
+          </StyledEditButtonWrapper>
+        </StyledNameAndButtonWrapper>
+      )}
+    </StyledWrapper>
+  );
+};
+
+const StyledWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledCheckboxWrapper = styled.div`
+  margin-right: 10px;
+`;
+
+const StyledNameAndButtonWrapper = styled.div`
+  display: flex;
+  margin-left: 170px;
+`;
+
+const StyledTaskName = styled.div``;
+
+const StyledEditButtonWrapper = styled.div``;
+export default Title;
