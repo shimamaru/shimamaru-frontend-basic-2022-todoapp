99674f2cdafd92f8ee32b419c71ae6887f1280f9
島谷　悠 <y.shimatani.22.10.658@4nonome.com>
2023-02-19 16:42:20 +0900

    スタイルの調整とタスク名の表示

--- src/components/Organisms/TodoCard/index.jsx
+++ src/components/Organisms/TodoCard/index.jsx
@@ -3,8 +3,10 @@ import { useState } from "react";
 import styled from "styled-components";
 import AddTaskButton from "../../Atoms/AddTaskButton";
 import Task from "../../Molecules/Task";
-
+import COLOR from "../../../variables/color";
 const TodoCard = () => {
+  const [taskList, setTaskList] = useState([]);
+
   const onAddTaskButtonClick = () => {
     setTaskList((prevTaskList) => [
       ...prevTaskList,
@@ -15,8 +17,6 @@ const TodoCard = () => {
   const onTaskComplete = (index) => {
     setTaskList((prevTaskList) => prevTaskList.filter((_, i) => i !== index));
   };
-
-  const [taskList, setTaskList] = useState([{ name: "", initializing: false }]);
 
   const onTaskNameChange = (value, index) => {
     setTaskList((prevTaskList) => {
@@ -38,7 +38,7 @@ const TodoCard = () => {
           <Task
             key={index}
             onTaskComplete={() => onTaskComplete(index)}
-            onTaskNameChange={(value) => onTaskNameChange(value, index)}
+            onTaskChange={(value) => onTaskNameChange(value, index)}
             taskName={task.name}
             defaultIsEditing={task.initializing}
           />
@@ -48,11 +48,21 @@ const TodoCard = () => {
   );
 };
 
-const StyledWrapper = styled.div``;
+const StyledWrapper = styled.div`
+  background-color: ${COLOR.LIGHT_BLACK};
+  border-radius: 4px;
+  padding: 22px 26px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 9px;
+`;
 
 const StyledTaskList = styled.ul`
   margin: 0;
   padding: 0;
+  display: flex;
+  flex-direction: column;
+  row-gap: 9px;
 `;
 
 export default TodoCard;
